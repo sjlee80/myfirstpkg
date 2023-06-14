285cac0028be46666bbb69b28d81ee7a32f451b5
root <root@Main-PC>
2023-06-14 20:08:00 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sjlee80/myfirstpkg
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package myfirstpkg
+
+import "fmt"
+
+func myFirstFunc() {
+	fmt.Println("출력안됨!")
+}
+
+func MyFirstFunc() {
+	fmt.Println("Hello, world!")
+}
